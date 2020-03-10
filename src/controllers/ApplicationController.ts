78e20ca94830de8cba0744afb7e813b796248680
csciubbach <csciubba@companieshouse.gov.uk>
2020-03-10 16:07:02 +0000

    Added axios instance

--- src/controllers/ApplicationController.ts
+++ src/controllers/ApplicationController.ts
@@ -5,6 +5,13 @@ class ApplicationController {
     public static getCreateNewApplication(request: Request, response: Response) {
         return response.render("register");
     }
+
+    public static postCreateNewApplication(request: Request, response: Response) {
+
+        // request.body
+
+        return response.render("register");
+    }
 }
 
 export default ApplicationController;
