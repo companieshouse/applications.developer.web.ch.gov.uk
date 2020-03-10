2affc71e6022a598ba73acc5ade46e9b7b80b67c
csciubbach <csciubba@companieshouse.gov.uk>
2020-03-10 15:44:45 +0000

    Added views folder

--- src/controllers/ApplicationController.ts
+++ src/controllers/ApplicationController.ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express";
 class ApplicationController {
 
     public static getCreateNewApplication(request: Request, response: Response) {
-        return response.send("Hi");
+        return response.render("register");
     }
 }
 
