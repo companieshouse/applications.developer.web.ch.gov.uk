5f61682cd0bfb57de0144f3a73bfdb996431d31f
Jonathan Blake <jblake2@companieshouse.gov.uk>
2020-07-23 09:39:53 +0100

    Added new start and routes file

---
+++ start.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Start script for applications.developer.identity.ch.gov.uk
+
+APP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [[ -z "${MESOS_SLAVE_PID}" ]]; then
+    source ~/.chs_env/private_env
+    source ~/.chs_env/global_env
+    source ~/.chs_env/applications.developer.identity.ch.gov.uk/env
+
+    exec npm run watch
+else
+    PORT="$1"
+    CONFIG_URL="$2"
+    ENVIRONMENT="$3"
+    APP_NAME="$4"
+
+    source /etc/profile
+
+    echo "Downloading environment from: ${CONFIG_URL}/${ENVIRONMENT}/${APP_NAME}"
+    wget -O "${APP_DIR}/private_env" "${CONFIG_URL}/${ENVIRONMENT}/private_env"
+    wget -O "${APP_DIR}/global_env" "${CONFIG_URL}/${ENVIRONMENT}/global_env"
+    wget -O "${APP_DIR}/app_env" "${CONFIG_URL}/${ENVIRONMENT}/${APP_NAME}/env"
+    source "${APP_DIR}/private_env"
+    source "${APP_DIR}/global_env"
+    source "${APP_DIR}/app_env"
+
+    cd ${APP_DIR}
+    source "server/config/.env"
+    export NODE_PORT=${PORT}
+    node server.js
+fi
