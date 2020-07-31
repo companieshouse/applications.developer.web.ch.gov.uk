4ea4be147708d12c4f1ca6318cf52c89e44fd920
Zain-Abbas1 <zabbas@companieshouse.gov.uk>
2020-07-31 13:29:03 +0100

    feature: create add application page fix logic in nav-bar so that the correct page is highlighted when on it

--- server/routes/applications.js
+++ server/routes/applications.js
@@ -9,12 +9,29 @@ const routeViews = 'applications';
 
 router.get('/', (req, res, next) => {
   logger.info(`GET request to serve index page: ${req.path}`);
-  res.render(`${routeViews}/index.njk`);
+  let viewData = {
+    this_data: {
+      active_page: 'application-overview'
+    },
+    this_errors: {}
+  }
+  res.render(`${routeViews}/index.njk`, viewData);
 });
 
 router.get('/manage-applications/add', (req, res, next) => {
-  logger.info(`GET request to serve index page: ${req.path}`);
-  res.render(`${routeViews}/index.njk`);
+  logger.info(`GET request to serve add application page: ${req.path}`);
+  let viewData = {
+    this_data: {
+      active_page: 'add-application'
+    },
+    this_errors: {}
+  }
+  res.render(`${routeViews}/add.njk`, viewData);
+});
+
+router.post('/manage-applications/add', (req, res, next) => {
+  logger.info(`POST request to process add application page: ${req.path}`);
+  res.redirect(302, '/');
 });
 
 router.get('/manage-applications/:appId/view', (req, res, next) => {
