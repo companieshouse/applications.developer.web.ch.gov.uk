26bac8eb0f1d231dd2ab6f3eee95076293bc6b30
Zain-Abbas1 <zabbas@companieshouse.gov.uk>
2020-08-21 13:30:10 +0100

    feature: intital commit for unit tests

--- test/server/routes/applications.test.js
+++ test/server/routes/applications.test.js
@@ -1,13 +1,15 @@
 const logger = require(`${serverRoot}/config/winston`);
 const Utility = require(`${serverRoot}/lib/Utility`);
-const Session = require(`${serverRoot}/lib/Session`);
+// const Session = require(`${serverRoot}/lib/Session`);
 let stubLogger;
 
 const errorManifest = require(`${serverRoot}/lib/errors/error_manifest`).validation;
 const Validator = require(`${serverRoot}/lib/validation`);
 const validator = new Validator();
-
+const ApplicationsDeveloperService = require(`${serverRoot}/services/ApplicationsDeveloper`);
+const applicationsDeveloperService = new ApplicationsDeveloperService();
 const app = require(`${serverRoot}/app`);
+const serviceData = require(`${testRoot}/server/_fakes/data/services/manage_applications`);
 
 const cookieStr = 'AD_SID=abc123';
 
@@ -26,14 +28,17 @@ describe('routes/report', () => {
     done();
   });
 
-  it.skip('should serve up the applications page with no mount path', () => {
-    const slug = '/';
+  it.only('should serve up the applications page with no mount path', () => {
+    const slug = '/manage-applications';
     return request(app)
       .get(slug)
       .set('Cookie', cookieStr)
       .then(response => {
+        const stubApplicationsDeveloperService = sinon.stub(ApplicationsDeveloperService.prototype, 'getList')
+          .returns(Promise.resolve(serviceData.getList));
         expect(response).to.have.status(200);
-        expect(stubLogger).to.have.been.calledOnce;
+        expect(stubLogger).to.have.been.called;//Look at this
+        expect(stubApplicationsDeveloperService).to.have.been.calledThrice;
       });
   });
 
