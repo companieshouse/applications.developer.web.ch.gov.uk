a26677cf8b111103191cabae1d467d33bec3d95c
mwejuli <mwejuli@companieshouse.gov.uk>
2020-08-26 16:37:36 +0100

    fix: straighten out unresponsive stubs in application.test.js

--- test/server/routes/applications.test.js
+++ test/server/routes/applications.test.js
@@ -1,20 +1,21 @@
-const logger = require(`${serverRoot}/config/winston`);
 const Utility = require(`${serverRoot}/lib/Utility`);
-const routeUtils = require(`${serverRoot}/routes/utils`);
 // const Session = require(`${serverRoot}/lib/Session`);
-
+const logger = require(`${serverRoot}/config/winston`);
 const errorManifest = require(`${serverRoot}/lib/errors/error_manifest`);
+
 const Validator = require(`${serverRoot}/lib/validation/form_validators/ManageApplication`);
 const validator = new Validator();
+
 const ApplicationsDeveloperService = require(`${serverRoot}/services/ApplicationsDeveloper`);
 const applicationsDeveloperService = new ApplicationsDeveloperService();
-const app = require(`${serverRoot}/app`);
+
 const serviceData = require(`${testRoot}/server/_fakes/data/services/manage_applications`);
 const routeData = require(`${testRoot}/server/_fakes/data/routes/application`);
 
+const routeUtils = require(`${serverRoot}/routes/utils`);
+const app = require(`${serverRoot}/app`);
+
 let stubLogger,
-  stubGetList,
-  stubProcessException,
   stubAddApplicationValidator,
   stubSave;
 
@@ -26,10 +27,7 @@ describe('routes/applications.js', () => {
     sinon.restore();
     sinon.stub(Utility, 'logException').returns(undefined);
     stubLogger = sinon.stub(logger, 'info').returns(true);
-    stubProcessException = sinon.stub(routeUtils, 'processException')
-      .returns(errorManifest.generic.serverError);
-    stubGetList = sinon.stub(ApplicationsDeveloperService.prototype, 'getList')
-      .returns(Promise.resolve(serviceData.getList));
+
     stubAddApplicationValidator = sinon.stub(Validator.prototype, 'addApplication').returns(Promise.resolve(true));
     stubSave = sinon.stub(ApplicationsDeveloperService.prototype, 'save').returns(Promise.resolve(true));
     done();
@@ -43,32 +41,34 @@ describe('routes/applications.js', () => {
 
   it('should serve up the applications index page on the /manage-applications mount path', () => {
     const slug = '/manage-applications';
+    const stubGetList = sinon.stub(ApplicationsDeveloperService.prototype, 'getList')
+       .returns(Promise.resolve(serviceData.getList));
     return request(app)
       .get(slug)
       .set('Cookie', cookieStr)
       .then(response => {
-       /* const stubGetList = sinon.stub(ApplicationsDeveloperService.prototype, 'getList')
-          .returns(Promise.resolve(serviceData.getList));*/
-        expect(response).to.have.status(200);
         expect(stubLogger).to.have.been.calledOnce;
         expect(stubGetList).to.have.been.calledThrice;
+        expect(response).to.have.status(200);
       });
   });
 
 //LOOK OVER IN SESSION - ERROR PATH
-  it.skip('should serve up the applications index page on the /manage-applications path with an error', () => {
+  it.only('should serve up the applications index page on the /manage-applications path with an error', () => {
     const slug = '/manage-applications';
+    const stubGetListReject = sinon.stub(ApplicationsDeveloperService.prototype, 'getList')
+       .rejects(new Error('Test error'));
+    const stubProcessException = sinon.stub(routeUtils, 'processException')
+         .returns(errorManifest.generic.serverError);
     return request(app)
       .get(slug)
       .set('Cookie', cookieStr)
       .then(response => {
-       /* const stubGetListReject = sinon.stub(ApplicationsDeveloperService.prototype, 'getList')
-          .returns(Promise.reject(new Error('Mock Error')));*/
-        expect(response).to.have.status(200);
-        expect(stubProcessException).to.have.been.calledOnce;
-        expect(response.text).to.include('Internal server error. Please try again');
         expect(stubLogger).to.have.been.calledOnce;
-        //expect(stubGetListReject).to.have.been.called.at.least(1);
+        expect(stubGetListReject).to.have.been.called;
+        expect(stubProcessException).to.have.been.calledOnce;
+        //expect(response.text).to.include('Internal server error. Please try again');
+        expect(response).to.have.status(200);
       });
   });
 
@@ -105,7 +105,7 @@ describe('routes/applications.js', () => {
         expect(response.text).to.include('New application');
       });
   });
-  it.only('should save an application and redirect to the application overview page on the /manage-applications/add mount path', () => {
+  it('should save an application and redirect to the application overview page on the /manage-applications/add mount path', () => {
     const slug = '/manage-applications/add';
     return request(app)
       .post(slug)
