1ddf66f4703430458b3948173410431268de5d2a
nabeelamirch <namir@companieshouse.gov.uk>
2020-09-16 09:25:46 +0100

    reverted optional chaining as it is only available in node 14

--- server/routes/utils/authentication.js
+++ server/routes/utils/authentication.js
@@ -2,7 +2,15 @@ const Utility = require(`${serverRoot}/lib/Utility`);
 
 const authentication = (req, res, next) => {
   try {
-    if (req.session?.data?.signin_info?.signed_in === 1) {
+    let authCheck = false;
+    if (typeof req.session !== 'undefined') {
+      if (typeof req.session.data.signin_info !== 'undefined') {
+        if (req.session.data.signin_info.signed_in === 1) {
+          authCheck = true;
+        }
+      }
+    }
+    if (authCheck) {
       next();
     } else {
       const port = req.socket.localPort && req.socket.localPort !== '80' ? ':' + req.socket.localPort : '';
