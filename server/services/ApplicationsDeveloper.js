9c737362e1f802b81cc82390bd9f70e5db9c4c54
ch-rfishwick <rfishwick@companieshouse.gov.uk>
2020-11-02 12:10:36 +0000

    fix: formatting issue

--- server/services/ApplicationsDeveloper.js
+++ server/services/ApplicationsDeveloper.js
@@ -170,7 +170,7 @@ class ApplicationsDeveloper {
   }
 
   async updateKey (data, appId, keyId, keyType, oauthToken, environment) {
-    if (keyType === 'key'){
+    if (keyType === 'key') {
       return this.updateRestApiKey(data, appId, keyId, oauthToken, environment)
     } else if (keyType === 'web') {
       return this.updateWebKey(data, appId, keyId, oauthToken, environment)
