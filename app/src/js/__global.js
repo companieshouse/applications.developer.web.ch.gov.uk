783c050498b921e7853511ad3ac2c4e057184ae3
ICraig-CH <icraig@companieshouse.gov.uk>
2020-11-24 16:22:31 +0000

    kept changes from cdn pr

--- app/src/js/__global.js
+++ app/src/js/__global.js
@@ -1,50 +1,46 @@
-$(document).ready(function (event) {
-  $('input[name=environment]').on('change', function (e) {
+$(document).ready(function(event) {
+  $("input[name=environment]").on("change", function (e) {
     showInDevelopmentOption();
   });
   (function () {
     showInDevelopmentOption();
   })();
 });
-
-function showInDevelopmentOption () {
-  if ($('input[name=environment]').prop('checked')) {
-    $('#conditional-environment').show();
+function showInDevelopmentOption() {
+  if ($("input[name=environment]").prop("checked")) {
+    $("#conditional-environment").show();
   } else {
-    $('#conditional-environment').hide();
+    $("#conditional-environment").hide();
   }
 }
-
-function clearDivs () {
+function clearDivs() {
   document.getElementById('restricted-ips').style.display = 'none';
   document.getElementById('redirect-uris').style.display = 'none';
   document.getElementById('js-domains').style.display = 'none';
 }
-function restClick () {
+function restClick() {
   document.getElementById('more-detail-uri').value = '';
   clearDivs();
   showElementById('restricted-ips');
   showElementById('js-domains');
 }
-function streamClick () {
+function streamClick() {
   document.getElementById('more-detail-uri').value = '';
   document.getElementById('more-detail-domain').value = '';
   clearDivs();
   showElementById('restricted-ips');
 }
-function webClick () {
+function webClick() {
   document.getElementById('more-detail-domain').value = '';
   document.getElementById('more-detail-ip').value = '';
   clearDivs();
   showElementById('redirect-uris');
 }
-
-function showElementById (id) {
+function showElementById(id) {
   document.getElementById(id).style.visibility = 'visible';
   document.getElementById(id).style.display = 'block';
 }
-
-function changeFieldSecurity (fieldId) {
+function changeFieldSecurity(fieldId) {
   var displayType = document.getElementById(fieldId).getAttribute('type');
   var linkId = fieldId + '-link';
   if (displayType === 'password') {
