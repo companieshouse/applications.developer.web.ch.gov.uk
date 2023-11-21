92554b300eaf9120f8a73d6765cb2224103b01ff
Harish Marimuthu <hmarimuthu@companieshouse.gov.uk>
2023-11-21 13:19:19 +0000

    CC-667 Polishing the spaces in the files.

--- terraform/groups/ecs-service/locals.tf
+++ terraform/groups/ecs-service/locals.tf
@@ -1,25 +1,25 @@
 # Define all hardcoded local variable and local variables looked up from data resources
 locals {
-  stack_name                = "developer-site" # this must match the stack name the service deploys into
-  name_prefix               = "${local.stack_name}-${var.environment}"
-  global_prefix             = "global-${var.environment}"
-  service_name              = "applications-developer"
-  container_port            = "3000" # default node port required here until prod docker container is built allowing port change via env var
-  docker_repo               = "applications.developer.web.ch.gov.uk"
+  stack_name                  = "developer-site" # this must match the stack name the service deploys into
+  name_prefix                 = "${local.stack_name}-${var.environment}"
+  global_prefix               = "global-${var.environment}"
+  service_name                = "applications-developer"
+  container_port              = "3000" # default node port required here until prod docker container is built allowing port change via env var
+  docker_repo                 = "applications.developer.web.ch.gov.uk"
   kms_alias                   = "alias/${var.aws_profile}/environment-services-kms"
-  lb_listener_rule_priority = 5
-  lb_listener_paths         = ["/manage-applications*"]
-  healthcheck_path          = "/manage-applications"
-  healthcheck_matcher       = "302" # no explicit healthcheck in this service yet, change this when added!
-  vpc_name = data.aws_ssm_parameter.secret[format("/%s/%s", local.name_prefix, "vpc-name")].value
-  s3_config_bucket          = data.vault_generic_secret.shared_s3.data["config_bucket_name"]
+  lb_listener_rule_priority   = 5
+  lb_listener_paths           = ["/manage-applications*"]
+  healthcheck_path            = "/manage-applications"
+  healthcheck_matcher         = "302" # no explicit healthcheck in this service yet, change this when added!
+  vpc_name                    = data.aws_ssm_parameter.secret[format("/%s/%s", local.name_prefix, "vpc-name")].value
+  s3_config_bucket            = data.vault_generic_secret.shared_s3.data["config_bucket_name"]
   app_environment_filename    = "applications.developer.web.ch.gov.uk.env"
   use_set_environment_files   = var.use_set_environment_files
-  application_subnet_ids    = data.aws_subnets.application.ids
-  application_subnet_pattern = local.stack_secrets["application_subnet_pattern"]
+  application_subnet_ids      = data.aws_subnets.application.ids
+  application_subnet_pattern  = local.stack_secrets["application_subnet_pattern"]
 
-  stack_secrets              = jsondecode(data.vault_generic_secret.stack_secrets.data_json)
-  service_secrets            = jsondecode(data.vault_generic_secret.service_secrets.data_json)
+  stack_secrets               = jsondecode(data.vault_generic_secret.stack_secrets.data_json)
+  service_secrets             = jsondecode(data.vault_generic_secret.service_secrets.data_json)
 
   # create a map of secret name => secret arn to pass into ecs service module
   # using the trimprefix function to remove the prefixed path from the secret name
@@ -62,7 +62,7 @@ locals {
   task_secrets = concat(local.global_secret_list,local.service_secret_list,[])
 
   task_environment = concat(local.ssm_global_version_map,local.ssm_service_version_map,[
-    { "name" : "LOGLEVEL", "value" : var.log_level },
+    { "name" : "LOGLEVEL",  "value" : var.log_level },
     { "name" : "NODE_PORT", "value" : local.container_port }
   ])
 
