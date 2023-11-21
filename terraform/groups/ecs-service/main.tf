d22fdd19051f3811cb41396e8ba6b960b1003cad
Harish Marimuthu <hmarimuthu@companieshouse.gov.uk>
2023-11-21 12:30:15 +0000

    CC-667 Refactoring to include recent features.

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -19,7 +19,7 @@ terraform {
 }
 
 module "ecs-service" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.215"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.221"
 
   # Environmental configuration
   environment             = var.environment
@@ -33,11 +33,12 @@ module "ecs-service" {
   lb_listener_arn           = data.aws_lb_listener.dev_site_lb_listener.arn
   lb_listener_rule_priority = local.lb_listener_rule_priority
   lb_listener_paths         = local.lb_listener_paths
-  healthcheck_path          = local.healthcheck_path
-  healthcheck_matcher       = local.healthcheck_matcher
+
 
   # ECS Task container health check
   use_task_container_healthcheck = true
+  healthcheck_path          = local.healthcheck_path
+  healthcheck_matcher       = local.healthcheck_matcher
 
   # Docker container details
   docker_registry   = var.docker_registry
@@ -49,21 +50,34 @@ module "ecs-service" {
   service_name = local.service_name
   name_prefix  = local.name_prefix
 
-  # Service Healthcheck configuration
-
   # Service performance and scaling configs
   desired_task_count                 = var.desired_task_count
   max_task_count                     = var.max_task_count
   required_cpus                      = var.required_cpus
   required_memory                    = var.required_memory
-  use_fargate                        = var.use_fargate
-  fargate_subnets                    = local.application_subnet_ids
   service_autoscale_enabled          = var.service_autoscale_enabled
   service_autoscale_target_value_cpu = var.service_autoscale_target_value_cpu
   service_scaledown_schedule         = var.service_scaledown_schedule
   service_scaleup_schedule           = var.service_scaleup_schedule
+  use_capacity_provider              = var.use_capacity_provider
+  use_fargate                        = var.use_fargate
+  fargate_subnets                    = local.application_subnet_ids
+
+  # Cloudwatch
+  cloudwatch_alarms_enabled = var.cloudwatch_alarms_enabled
 
   # Service environment variable and secret configs
   task_environment = local.task_environment
   task_secrets     = local.task_secrets
+  app_environment_filename    = local.app_environment_filename
+  use_set_environment_files   = local.use_set_environment_files
+}
+
+module "secrets" {
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.216"
+
+  name_prefix = "${local.service_name}-${var.environment}"
+  environment = var.environment
+  kms_key_id  = data.aws_kms_key.kms_key.id
+  secrets     = nonsensitive(local.service_secrets)
 }
